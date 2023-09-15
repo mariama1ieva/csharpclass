6a5214cd278f2b85165a405057047c4f8e4f139b
mariama1ieva <maryamfa@code.edu.az>
2023-09-15 15:05:33 +0400

    added tasks at class

---
+++ tasksclass/Program.cs
@@ -0,0 +1,88 @@
+﻿//int[] nums = { 1, 2, 3, 8, 6, 3, 9, 10, 3 };
+//for (int i = 0; i < nums.Length; i++) 
+//{
+//    if (nums[i] ==3)
+//    {
+//        Console.WriteLine(nums[i]);
+//        break;
+//    }
+//}
+
+//int[] nums = { 1, 2, 3, 8, 6, 3, 9, 10, 3 };
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i]==3)
+//    {
+//        Console.WriteLine(nums[i]);
+//    }
+//}
+
+//int day = 4;
+//switch(day)
+//{
+//    case 1:
+//        Console.WriteLine("monday");
+//        break;
+//        case 2:
+//        Console.WriteLine("tuesday");
+//        break;
+//        case 3:
+//        Console.WriteLine("wednesday");
+//        break;
+//        case 4:
+//        Console.WriteLine("thursday");
+//        break;
+//        case 5:
+//        Console.WriteLine("friday");
+//        break;
+//        case 6:
+//        Console.WriteLine("saturday");
+//        break;
+//}
+
+//using System.Diagnostics.CodeAnalysis;
+
+
+//int[] nums = { 1, 2, 3, 8, 6, 3, 9, 10, 3 };
+//int sumindex = 0;
+//int sumelem = 0;
+
+//for (int i = 0; i < nums.Length; i++)   
+//{
+//    if (nums[i] == 3)
+//    {
+//        sumindex += i;
+//        sumelem += nums[i];
+//    }
+
+//}
+//int result = sumelem + sumindex;
+//Console.WriteLine(result);
+
+
+//int[] nums = { 1, 2, 3, 8, 6, 3, 9, 10, 3 };
+//for (int i = 0;i<nums.Length;i++)
+//{
+//    if (nums[i] == 3)
+//    {
+//        continue;
+//    }
+//    Console.WriteLine(nums[i]);
+//}
+
+
+//using System.ComponentModel.Design;
+//Console.WriteLine("add youremail");
+//string email=Console.ReadLine();
+
+//Console.WriteLine("add your password");
+//string password=Console.ReadLine(); 
+
+//if(email== "meryem@gmail.com" && password=="1223")
+//{
+//    Console.WriteLine("login success");
+//}
+//else
+//{
+//    Console.WriteLine("password or email is wrong");
+//}
